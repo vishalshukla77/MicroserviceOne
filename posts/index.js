597b46a5eab77e5d62fd1c174dd4263f131af57c
vishalshukla77 <121348446+vishalshukla77@users.noreply.github.com>
2025-05-28 11:55:57 +0530

    added final commit

--- posts/index.js
+++ posts/index.js
@@ -1,28 +1,26 @@
-const express= require('express');
-const bodyParser=require('body-parser')
-const { randomBytes } = require('crypto'); 
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
-const app=express();
+const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const posts={};
+const posts = {};
 
-app.get("/posts",(req,res)=>{
+app.get("/posts", (req, res) => {
+  res.send(Object.values(posts));  // Return an array
+});
 
-    res.send(posts);
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = { id, title };
+  console.log("New post added:", posts[id]); // Debug log
+  res.status(201).send(posts[id]);
+});
 
-})
-app.post("/posts",(req,res)=>{
-    const id=randomBytes(4).toString('hex');
-    const {title}=req.body;
-    posts[id]={
-        id,title
-    };
-
-    res.status(201).send(posts[id]);
-    
-})
-
-app.listen(4000,()=>{
-    console.log("listening on 4000");
-})
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
